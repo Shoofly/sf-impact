2c693f98bec310ee685eb850747708689c8ab284
Adrian <adrian@shooflysolutions.com>
2015-10-14 11:40:33 -0400

    Fix bad merge

--- header.php
+++ header.php
@@ -24,8 +24,6 @@
         $url = sf_impact_getCustomUrl($url);    
   
     }
-                     
-                     
                 
     //Get the settings for the header
     $sf_impact_logo_location = get_theme_mod('sf_impact_logo_location', 'image');
@@ -139,124 +137,14 @@
                 if ( $url )
                 {
                     ?>
-    //Get the settings for the header
-    $sf_impact_logo_location = get_theme_mod('sf_impact_logo_location', 'image');
-    $sf_impact_menu_location = get_theme_mod('sf_impact_menu_location', 'above');
-    $sf_impact_home_header_type = get_theme_mod('sf_impact_home_header_type', '3');
-    $sf_impact_header_image = get_theme_mod('sf_impact_header_image', '');
-    $sf_impact_social_above_content = get_theme_mod('sf_impact_social_above_content', true);
-    $sf_impact_social_above_menu = get_theme_mod('sf_impact_social_above_menu', false);
-    //Is this the home page or the front page (and not the blog page) and the header type is not default display the custom home page header image/slide show
-    if ((is_home() || is_front_page()) && $sf_impact_home_header_type != "3" && !$wp_query -> is_posts_page)
-        $homeimage = TRUE;      //Display the custom home page header
-    else
-        $homeimage = FALSE;     //Do not display the custom home page header
-    
-    $logo = $sf_impact_logo_location; 
-    $menu = $sf_impact_menu_location;
-    $the_slide_query  = NULL;
-    $noheaderimg = FALSE;
-
-    if ($homeimage) //This check is only going to happen for the home page or the front page
-    {
-        $noheaderimg = FALSE;
-        switch ($sf_impact_home_header_type)
-        {
-            default:
-            case NOHEADER:
-                $noheaderimg = TRUE;
-                break;
-            case DEFAULTHEADER:
-                if ($url == "")
-                    $noheaderimg = TRUE;
-                break;
-            case CUSTOMHEADER:
-                 if ($sf_impact_header_image=="")
-                    $noheaderimg = TRUE;
-                break;
-            case SLIDEHEADER:
-                $the_slide_query = sf_impact_slideshow_query(); //Make sure that there are slides
-               
-                if ($the_slide_query->post_count <= 0)
-                { 
+<<<<<<< HEAD
             
-                    $sf_impact_home_header_type = 2;
-                    $noheaderimg = TRUE;
-                    $the_slide_query = NULL;
-                }
-                break;
-        }
-    } 
-     elseif (!$url) { //This check only occurs on pages that are not the front page or home page 
-        $noheaderimg = TRUE;
-    }
-    if ($noheaderimg)
-    {
-        $logo="top";
-        $menu = "above";
-        
-    }      
-?>
-
-<body <?php body_class();?>><div id="page" class="hfeed site  clear">
-	
-       
-        <div id="topmasthead" class="fixed"> <!-- navigatin area -->
-            <div id="containermasthead">
-                <?php if ($sf_impact_social_above_menu) sf_impact_social_media_icons();?> 
-            <div id="outermasthead">
-            
-                <div id="innermasthead" class="fixed">
-                <?php
-                if ($logo == "top")   //If the logo or title is on top, display it here 
-                {
-                    ?>
-                    <div class="site-branding fixed" >
-                    <?php
-                    get_template_part('template-parts/branding');
-                    ?>
-                    </div>               
-                <?php
-            
-                }?>
-  
-                <?php if ($menu == "above")
-                    sf_impact_menu();
-                ?>
- 	    
-                   
-                </div><!--innermasthead-->
-        </div>
-            </div>
-        </div><!--topmasthead-->
- 	<header id="masthead" class="site-header" role="banner">
-        <?php
-        if ($homeimage === TRUE)
-            $xClass="sfly-headerimg-home";
-        else
-            $xClass = "";
-        ?>
-            
-        <div class="sfly-headerimg <?php echo $xClass; ?>">
-            <?php    
-             if ($logo=="image")
-            {?>
-                <div class="site-branding fixed shoofly-branding-image" >
-                    <?php get_template_part('template-parts/branding');?>
-		        </div>
-             <?php 
-            }
-            if (!$homeimage) //If this is not the home image or the front page, display the url
-            {
-                if ( $url )
-                {
-                    ?>
-                    <div class="header-container-inner">
-            
-                        <img class="headerimg headerimg-page" src="<?php echo $url; ?>" alt="" />
                     <img class="headerimg headerimg-page" src="<?php echo $url ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
-                    </div><?php  
-                <?php  
+                  
+                    <div class="header-container-inner">
+                        <img class="headerimg headerimg-page" src="<?php echo $url; ?>" alt="" >
+                    </div>
+                <?php      
                 }
             }
             else
@@ -312,4 +200,4 @@
 	</header><!-- #masthead -->
     
 	<div id="content" class="site-content">
-     <?php if ($sf_impact_social_above_content) sf_impact_social_media_icons(); ?>     
+     <?php if ($sf_impact_social_above_content) sf_impact_social_media_icons(); ?>
