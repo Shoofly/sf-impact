663d900344d5aa15667428483ada6d505ab0eb26
Adrian <adrian@shooflysolutions.com>
2015-10-14 20:30:43 -0400

    Same

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -235,7 +235,7 @@ if ( ! function_exists( 'sf_impact_entry_footer' ) ) :
 		    }
 	    }
     
-        $sfly_theme1_show_author = get_theme_mod('sf_impact_show_author', true);
+        $sf_impact_show_autho = get_theme_mod('sf_impact_show_author', true);
    	    if ( is_single() &&   $sf_impact_show_author  ) :
 			    get_template_part( 'template-parts/author-bio' );
 		    endif;
